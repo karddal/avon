edd82c2620c2442a35a46023d4e1a4258ca28b22
rsh <emkayhrushikesh@gmail.com>
2025-10-16 22:04:28 +0100

    Added in tabs to seperate finished units and ongoing units. Need to add the logic to differentiate both units

--- frontend/src/app/units/page.tsx
+++ frontend/src/app/units/page.tsx
@@ -4,8 +4,19 @@
 import Unit from "@/components/unit";
 import YearSelector from "@/components/year-selector";
 
+import {
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from "@/components/ui/tabs"
+import { useState } from "react";
+
 export default function UnitPage() {
+	const [isOpen, setIsOpen] = useState(false)
+
 	const apiCall = [
+
 		{ name: "Imperative and Functional Programming", code: "100016", year: 2025, finished: false, color: "blue", mark: 0, courseworkLive: false },
 		{ name: "Computer Architecure", code: "100015", year: 2025, finished: false, color: "amber", mark: 0, courseworkLive: false },
 		{ name: "Mathematics for Computer Science", code: "100014", year: 2025, finished: false, color: "teal", mark: 0, courseworkLive: false },
@@ -13,15 +24,35 @@ export default function UnitPage() {
 		{ name: "Software Tools", code: "100012", year: 2025, finished: false, color: "rose", mark: 0, courseworkLive: false },
 		{ name: "Mathematics for Computer Science B", code: "100013", year: 2025, finished: false, color: "purple", mark: 0, courseworkLive: false },
 	]
-	const units = apiCall.map((unit) => <Unit key={unit.code} props={unit} />)
+	const ongoingUnits = apiCall.map((unit) =>
 
+		<Unit key={unit.code} props={unit} />
+	)
+	const finishedUnits = apiCall.map((unit) => <Unit key={unit.code} props={unit} />)
 	return (
 		<>
 			<div className="space-y-6">
 				<YearSelector />
-				<section className="grid gap-4 grid-cols-2">
-					{units}
-				</section>
+				{/* Ongoing Units */}
+
+				<div>
+					<Tabs defaultValue="account">
+						<TabsList>
+							<TabsTrigger value="ongoing">Ongoing</TabsTrigger>
+							<TabsTrigger value="finished">Finished</TabsTrigger>
+						</TabsList>
+						<TabsContent value="ongoing">
+							<section className="grid gap-4 grid-cols-2">
+								{ongoingUnits}
+							</section>
+						</TabsContent>
+						<TabsContent value="finished">
+							<section className="grid gap-4 grid-cols-2">
+								{finishedUnits}
+							</section>
+						</TabsContent>
+					</Tabs>
+				</div>
 			</div>
 		</>
 	);
