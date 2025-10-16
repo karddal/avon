6d28f7e019be9d0dadbc8b5bae961da0b225e84a
Jack Dempsey <jack.william.dempsey@gmail.com>
2025-10-16 23:22:26 +0100

    adding in courseworkId

--- frontend/src/app/courseworks/page.tsx
+++ frontend/src/app/courseworks/page.tsx
@@ -5,17 +5,17 @@ export default function courseworkListing() {
     
     const apiCall = [
 
-		{ name: "List", code: "100016", year: 2025, finished: true, color: "blue", dueDate: "20/9/2025", testsPassed: 50, totalTests: 50},
-		{ name: "Sketch", code: "100016", year: 2025, finished: true, color: "amber", dueDate: "20/12/2025", testsPassed: 58, totalTests: 58},
-		{ name: "Power to the People", code: "100016", year: 2025, finished: false, color: "teal", dueDate: "18/10/2025", testsPassed: 13, totalTests: 68},
-		{ name: "Scotland Yard", code: "100018", year: 2025, finished: false, color: "emerald", dueDate: "16/11/2025", testsPassed: 21, totalTests: 43},
-		{ name: "Simplify", code: "100016", year: 2025, finished: false, color: "rose", dueDate: "5/10/2025", testsPassed: 46, totalTests: 74},
+		{ courseworkId: "112354", name: "List", code: "100016", year: 2025, finished: true, color: "blue", dueDate: "20/9/2025", testsPassed: 50, totalTests: 50},
+		{ courseworkId: "347483", name: "Sketch", code: "100016", year: 2025, finished: true, color: "amber", dueDate: "20/12/2025", testsPassed: 58, totalTests: 58},
+		{ courseworkId: "566567", name: "Power to the People", code: "100016", year: 2025, finished: false, color: "teal", dueDate: "18/10/2025", testsPassed: 13, totalTests: 68},
+		{ courseworkId: "886567", name: "Scotland Yard", code: "100018", year: 2025, finished: false, color: "emerald", dueDate: "16/11/2025", testsPassed: 21, totalTests: 43},
+		{ courseworkId: "977557", name: "Simplify", code: "100016", year: 2025, finished: false, color: "rose", dueDate: "5/10/2025", testsPassed: 46, totalTests: 74},
 	]
     const ongoing = apiCall.filter(unit => unit.finished === false)
-	const ongoingUnits = ongoing.map((unit) => <Coursework key={unit.code} props={unit} />)
+	const ongoingUnits = ongoing.map((unit) => <Coursework key={unit.courseworkId} props={unit} />)
 
     const finished = apiCall.filter(unit => unit.finished === true)
-	const finishedUnits = finished.map((unit) => <Coursework key={unit.code} props={unit} />)
+	const finishedUnits = finished.map((unit) => <Coursework key={unit.courseworkId} props={unit} />)
 
     return (
         <div className="space-y-6">
