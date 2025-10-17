8843b7d36683fa785da02f126bef24962f55ed73
Josh Jenkins <josshuaj7@gmail.com>
2025-10-17 19:18:08 +0100

    Tabs added to lecturer dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import { StatsChart } from "@/components/stats-chart";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Tabs, TabsContent, TabsTrigger, TabsList } from "@/components/ui/tabs";
 
 export default function DashboardPage() {
   return (
@@ -92,50 +93,6 @@ export default function DashboardPage() {
           </CardContent>
         </Card>
         <div className="col-span-3 flex lg:flex-row flex-col gap-4">
-          <Card className="flex flex-col lg:w-[70%]">
-            <CardTitle>
-              <div className="font-semibold text-2xl px-6">Coursework</div>
-              <div className="font-light text-l px-6">
-                See your currently assigned courseworks here.
-              </div>
-            </CardTitle>
-            <CardContent>
-              <div className="gap-2 overflow-y-auto flex flex-col h-96">
-                <Suspense fallback={<Skeleton />}>
-                  <Link href="/coursework/power-to-the-people">
-                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
-                      <p className="p-4">Power to the People</p>
-                      <div className="w-full h-1 bg-pink-500"></div>
-                    </Card>
-                  </Link>
-                  <Link href="/coursework/simplify">
-                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
-                      <p className="p-4">Simplify</p>
-                      <div className="w-full h-1 bg-pink-500"></div>
-                    </Card>
-                  </Link>
-                  <Link href="/coursework/list">
-                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
-                      <p className="p-4">List</p>
-                      <div className="w-full h-1 bg-blue-500"></div>
-                    </Card>
-                  </Link>
-                  <Link href="/coursework/sketch">
-                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
-                      <p className="p-4">Sketch</p>
-                      <div className="w-full h-1 bg-blue-500"></div>
-                    </Card>
-                  </Link>
-                  <Link href="/coursework/sketch">
-                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
-                      <p className="p-4">Sketch</p>
-                      <div className="w-full h-1 bg-blue-500"></div>
-                    </Card>
-                  </Link>
-                </Suspense>
-              </div>
-            </CardContent>
-          </Card>
           <Card className="flex flex-col lg:w-[30%]">
             <CardTitle>
               <div className="font-semibold text-2xl px-6">Quick Actions</div>
@@ -148,9 +105,7 @@ export default function DashboardPage() {
                 <Card className="bg-green-400/10 p-2">
                   <CardContent className="flex flex-row p-0 items-center gap-4">
                     <Plus className="size-12 p-0"></Plus>
-                    <p className="font-semibold text-xl">
-                      Create a new coursework
-                    </p>
+                    <p className="font-semibold text-xl">Create coursework</p>
                   </CardContent>
                 </Card>
               </Link>
@@ -159,9 +114,7 @@ export default function DashboardPage() {
                 <Card className="bg-red-400/10 p-2">
                   <CardContent className="flex flex-row p-0 items-center gap-4">
                     <Flag className="size-12 p-0"></Flag>
-                    <p className="font-semibold text-xl">
-                      End submissions to a coursework
-                    </p>
+                    <p className="font-semibold text-xl">End submissions</p>
                   </CardContent>
                 </Card>
               </Link>
@@ -175,6 +128,123 @@ export default function DashboardPage() {
               </Link>
             </CardContent>
           </Card>
+          <Card className="flex flex-col lg:w-full">
+            <CardContent>
+              <Tabs defaultValue="coursework" className="">
+                <TabsList>
+                  <TabsTrigger value="coursework">Coursework</TabsTrigger>
+                  <TabsTrigger value="units">Units</TabsTrigger>
+                </TabsList>
+                <div className="gap-2 overflow-y-auto flex flex-col h-96">
+                  <Suspense fallback={<Skeleton />}>
+                    <TabsContent
+                      className="flex flex-col gap-2"
+                      value="coursework"
+                    >
+                      <Link href="/coursework/power-to-the-people">
+                        <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                          <p className="p-4">Power to the People</p>
+                          <div className="w-full h-1 bg-pink-500"></div>
+                        </Card>
+                      </Link>
+                      <Link href="/coursework/simplify">
+                        <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                          <p className="p-4">Simplify</p>
+                          <div className="w-full h-1 bg-pink-500"></div>
+                        </Card>
+                      </Link>
+                      <Link href="/coursework/list">
+                        <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                          <p className="p-4">List</p>
+                          <div className="w-full h-1 bg-blue-500"></div>
+                        </Card>
+                      </Link>
+                      <Link href="/coursework/sketch">
+                        <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                          <p className="p-4">Sketch</p>
+                          <div className="w-full h-1 bg-blue-500"></div>
+                        </Card>
+                      </Link>
+                      <Link href="/coursework/sketch">
+                        <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                          <p className="p-4">Sketch</p>
+                          <div className="w-full h-1 bg-blue-500"></div>
+                        </Card>
+                      </Link>
+                    </TabsContent>
+                  </Suspense>
+                </div>
+              </Tabs>
+            </CardContent>
+          </Card>
+          {/* <Card className="flex flex-col lg:w-full">
+            <CardTitle>
+              <div className="font-semibold text-2xl px-6">Units</div>
+              <div className="font-light text-l px-6">
+                See your assigned units here.
+              </div>
+            </CardTitle>
+            <CardContent>
+              <div className="gap-2 overflow-y-auto flex flex-col h-96">
+                <Suspense fallback={<Skeleton />}>
+                  <Link href="/units/COMS10015">
+                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                      <p className="px-2 py-0 text-medium">
+                        Computer Architecture
+                      </p>
+                      <p className="px-2 py-0 text-sm font-light">COMS10015</p>
+                      <div className="w-full h-1 bg-green-500"></div>
+                    </Card>
+                  </Link>
+                  <Link href="/units/COMS10015">
+                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                      <p className="px-2 py-0 text-medium">
+                        Computer Architecture
+                      </p>
+                      <p className="px-2 py-0 text-sm font-light">COMS10015</p>
+                      <div className="w-full h-1 bg-green-500"></div>
+                    </Card>
+                  </Link>
+                  <Link href="/units/COMS10015">
+                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                      <p className="px-2 py-0 text-medium">
+                        Computer Architecture
+                      </p>
+                      <p className="px-2 py-0 text-sm font-light">COMS10015</p>
+                      <div className="w-full h-1 bg-green-500"></div>
+                    </Card>
+                  </Link>
+                  <Link href="/units/COMS10015">
+                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                      <p className="px-2 py-0 text-medium">
+                        Computer Architecture
+                      </p>
+                      <p className="px-2 py-0 text-sm font-light">COMS10015</p>
+                      <div className="w-full h-1 bg-green-500"></div>
+                    </Card>
+                  </Link>
+                  <Link href="/units/COMS10015">
+                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                      <p className="px-2 py-0 text-medium">
+                        Computer Architecture
+                      </p>
+                      <p className="px-2 py-0 text-sm font-light">COMS10015</p>
+                      <div className="w-full h-1 bg-green-500"></div>
+                    </Card>
+                  </Link>
+                  <Link href="/units/COMS10015">
+                    <Card className="gap-2 p-0 bg-accent w-full text-2xl font-normal hover:bg-foreground/10">
+                      <p className="px-2 py-0 text-medium">
+                        Computer Architecture
+                      </p>
+                      <p className="px-2 py-0 text-sm font-light">COMS10015</p>
+                      <div className="w-full h-1 bg-green-500"></div>
+                    </Card>
+                  </Link>
+                </Suspense>
+              </div>
+            </CardContent>
+          </Card> */}
         </div>
       </section>
     </div>
