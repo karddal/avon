d8d2cd68f7f2d25e5e27c140efe9d2abcff1c0b3
Josh Jenkins <josshuaj7@gmail.com>
2025-10-20 21:46:43 +0100

    Making a simple analytics page with a heatmap component I have made. Also removing overflow-y-scroll on CardContent as this is causing issues.

---
+++ frontend/src/app/analytics/layout.tsx
@@ -0,0 +1,23 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+          <ModeToggle />
+          <SidebarTrigger className="-ml-1" />
+          <p className="font-normal text-xl">Analytics</p>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 px-4">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
