bdef99d93d9af4a58f563cae4e5a07a8d9dc70f6
Josh Jenkins <josshuaj7@gmail.com>
2025-10-20 18:09:15 +0100

    Adding floating sidebar with large icons, works well on mobile - Slighly changed Sidebar.tsx as had to add a height functionality for the sidebar on mobile - also allow the sidebar to extend to full width on mobile screens between 640px and 768px

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,4 +1,12 @@
-import { GalleryVerticalEnd } from "lucide-react";
+import {
+  BookText,
+  ChartLine,
+  LayoutDashboard,
+  Settings,
+  SwatchBook,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import type * as React from "react";
 
 import {
@@ -9,72 +17,91 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 
-const data = {
-  navMain: [
-    {
-      title: "Units",
-      url: "#",
-      icon: "Units",
-      items: [
-        {
-          title: "Coursework",
-          url: "#",
-          icon: "Coursework",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-  ],
-};
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+    admin: true,
+  },
+  {
+    title: "Analytics",
+    url: "/analytics",
+    icon: ChartLine,
+    admin: true,
+  },
+  {
+    title: "Units",
+    url: "#",
+    icon: SwatchBook,
+    admin: false,
+  },
+  {
+    title: "Coursework",
+    url: "/courseworks",
+    icon: BookText,
+    admin: false,
+  },
+
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+    admin: false,
+  },
+];
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar variant="floating" {...props}>
       <SidebarHeader>
         <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <a href="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-medium">avon</span>
-                </div>
-              </a>
+          <SidebarMenuItem className="py-4 md:py-0">
+            <SidebarMenuButton
+              className="size-20 w-full justify-center md:justify-center md:aspect-square"
+              asChild
+            >
+              <div>
+                <Image
+                  src="/avon.png"
+                  alt="logo"
+                  width={200}
+                  height={200}
+                  className="dark:block hidden p-4 md:p-0"
+                />
+                <Image
+                  src="/avonlight.png"
+                  alt="logo"
+                  width={200}
+                  height={200}
+                  className="dark:hidden p-4 md:p-0"
+                />
+              </div>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarMenu className="gap-2">
-            {data.navMain.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <a href={item.url} className="font-medium">
-                    {item.title}
-                  </a>
+          <SidebarMenu className="flex flex-row flex-wrap justify-center items-center md:flex-col md:flex-wrap md:justify md:items-center">
+            {items.map((item) => (
+              <SidebarMenuItem
+                className=" aspect-square
+                md:flex-1 md:!h-full md:!w-full"
+                key={item.title}
+              >
+                <SidebarMenuButton className="h-full" asChild>
+                  <Link
+                    className="flex flex-col justify-center items-center
+                    !h-32 !w-32 md:!h-full md:!w-full aspect-square"
+                    href={item.url}
+                  >
+                    <item.icon size={100} className="!size-12" />
+                    <span className="text-accent-foreground">{item.title}</span>
+                  </Link>
                 </SidebarMenuButton>
-                {item.items?.length ? (
-                  <SidebarMenuSub className="ml-0 border-l-0 px-1.5">
-                    {item.items.map((item) => (
-                      <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
-                          <a href={item.url}>{item.title}</a>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                ) : null}
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
