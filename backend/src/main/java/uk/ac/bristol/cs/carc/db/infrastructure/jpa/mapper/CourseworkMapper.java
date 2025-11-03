560a70ff4d666a96270ded146c7cec122b0e83b8
Mihaly Toth-Tarsoly <me@misi.me>
2025-11-03 13:44:31 +0000

    basic with dependencies installed

--- backend/src/main/java/uk/ac/bristol/cs/carc/db/infrastructure/jpa/mapper/CourseworkMapper.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.bristol.cs.carc.db.infrastructure.jpa.mapper;
-
-import org.mapstruct.Mapper;
-import uk.ac.bristol.cs.carc.db.domain.model.Coursework;
-import uk.ac.bristol.cs.carc.db.infrastructure.jpa.entity.CourseworkEntity;
-
-@Mapper(componentModel = "spring")
-public interface CourseworkMapper extends DomainEntityBaseMapper<Coursework, CourseworkEntity> {
-}
--- backend/src/main/java/uk/ac/bristol/cs/carc/user/UserModuleConfiguration.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.bristol.cs.carc.user;
-
-import org.springframework.modulith.ApplicationModule;
-
-@ApplicationModule(displayName = "Student Module")
-public class UserModuleConfiguration {}
