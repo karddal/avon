7bf0d91eace4a4cba9c992dacbbb150a0011dbc3
polo233473 <37165099+polo233473@users.noreply.github.com>
2025-11-10 20:19:14 +0000

    addUnitGroupModel
    add Unit Group Model and its custom type

---
+++ backend/app/core/types/academicYear.py
@@ -0,0 +1,15 @@
+
+
+class AcademicYear:
+    def __init__(self, start_year: int):
+        self.startYear = start_year
+        self.endYear = start_year + 1
+
+    def get_start_year(self) -> int:
+        return self.startYear
+
+    def get_end_year(self) -> int:
+        return self.endYear
+
+    def set_start_year(self, start_year: int):
+        self.startYear = start_year
