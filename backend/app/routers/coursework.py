f991b2f4e28f0c9a73d42ac01082e0a5da2412e9
Jack Dempsey <jack.william.dempsey@gmail.com>
2025-11-12 12:37:58 +0000

    making all routes and schemas

---
+++ backend/app/routers/coursework.py
@@ -0,0 +1,67 @@
+from http.client import responses
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import Session, select
+from app.db.session import get_session
+from typing import Annotated
+from uuid import UUID
+
+from app.models.coursework import Coursework
+from app.schemas.coursework import CourseworkCreate, CourseworkRead, CourseworkUpdate, CourseworkDelete
+
+router = APIRouter(prefix = "/coursework")
+session_dependency = Annotated[Session, Depends(get_session)]
+
+
+@router.post('/create', response_model = CourseworkRead, status_code=status.HTTP_201_CREATED)
+async def create_user(coursework: CourseworkCreate, session: session_dependency):
+    db_user = session.exec(select(Coursework).where((Coursework.unit_id == coursework.unit_id) and (Coursework.name == coursework.name))).first()
+    
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Coursework already made that belongs to the same unit and has the same name")
+    
+    db_coursework = Coursework(name=coursework.name,description=coursework.description,unit_id=coursework.unit_id, due_date=coursework.due_date)
+    session.add(db_coursework)
+    session.commit()
+    session.refresh(db_coursework)
+    return db_coursework
+
+@router.get('/{id}', response_model = CourseworkRead)
+async def get_coursework(id: UUID, session: session_dependency):
+    coursework = session.get(Coursework,id)
+
+    if coursework is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Coursework not found')
+    return coursework
+
+@router.delete('/{id}')
+async def delete(id: UUID, session: session_dependency):
+    coursework = session.get(Coursework,id)
+
+    if coursework is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Coursework not found')
+    
+    session.delete(coursework)
+    session.commit()
+    
+    return 
+
+@router.put('/{id}', response_model = CourseworkRead)
+async def update_user(id: UUID, coursework: CourseworkUpdate, session: session_dependency):
+    coursework_db = session.get(Coursework,id)
+
+    if coursework_db is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Coursework not found')
+    
+    if coursework.unit_id is not None:
+        unit_exists = session.exec(select(Unit).where(Unit.id == coursework.unit_id)).first()
+        if unit_exists:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Corresponding unit not found")
+
+    coursework_data = coursework.model_dump(exclude_unset=True)
+    coursework_db.sqlmodel_update(coursework_data)
+
+    session.add(coursework_db)
+    session.commit()
+    session.refresh(coursework_db)
+    return coursework_db
---
+++ backend/app/schemas/coursework.py
@@ -0,0 +1,61 @@
+from typing import Annotated
+
+import uuid
+import datetime
+from pydantic import BaseModel, AfterValidator
+from uuid import UUID
+
+
+def is_valid_name(name: str) -> str:
+    if 1 <= len(name) <= 100:
+        return name
+    else:
+        raise ValueError("Name must be between 1 and 100 characters")
+
+
+def is_valid_description(description: str) -> str:
+    if 1 <= len(description) <= 5000:
+        return description
+    else:
+        raise ValueError("Description must be between 1 and 5000 characters")
+
+
+def is_valid_due_date(date: datetime.datetime) -> datetime.datetime:
+    now = datetime.datetime.now()
+    one_year_onwards = now + datetime.timedelta(days=365)
+
+    if date <= now:
+        raise ValueError("Due date must be greater than today")
+    elif date > one_year_onwards:
+        raise ValueError("Due date must be within one year from now")
+    else:
+        return date
+
+
+# Type aliases
+Name = Annotated[str, AfterValidator(is_valid_name)]
+Description = Annotated[str, AfterValidator(is_valid_description)]
+DueDate = Annotated[datetime.datetime, AfterValidator(is_valid_due_date)]  # Fixed
+
+class CourseworkRead(BaseModel):
+    id: UUID
+    name: str
+    description: str
+    unit_id: UUID
+    due_date: datetime.datetime #already validated as from our api
+    creation_date: datetime.datetime
+
+class CourseworkCreate(BaseModel):
+    name: Name
+    description: Description
+    unit_id: UUID
+    due_date: DueDate
+
+
+class CourseworkUpdate(BaseModel):
+    id: UUID
+    name: Name | None = None
+    description: Description | None = None
+    unit_id: UUID | None = None
+    due_date: DueDate | None = None
+
