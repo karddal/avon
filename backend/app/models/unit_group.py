50e504e80187693afb9de30bb6d6db646417e0de
Josh Jenkins <josshuaj7@gmail.com>
2025-11-14 14:53:52 +0000

    Readding the back_populates for unit_group

--- backend/app/models/unit_group.py
+++ backend/app/models/unit_group.py
@@ -1,10 +1,15 @@
 import uuid
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from uuid import UUID
-
-
+from typing import List
+from app.models.unit import Unit
+from app.models.user_group_member import UserGroupMember
 
 class UnitGroup(SQLModel, table = True):
     id: UUID = Field(primary_key=True, default_factory=uuid.uuid4)
     name: str = Field(index = True)
     academic_year: int = Field(index = True)
+    unit: List[Unit] = Relationship(
+        back_populates="groups",
+        link_model=UserGroupMember
+    )
