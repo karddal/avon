d0c2f918db99e212adf66ab6abe12e9ff6d3a0e0
Jack Dempsey <jack.william.dempsey@gmail.com>
2026-01-02 21:48:56 +0000

    Testing framewotk and security tests done

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -1,10 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     jwt_secret_key: str
     access_token_expiry_minutes: int
-    class Config:
-        env_file = ".env"
-
+    model_config = SettingsConfigDict(env_file=".env") # Keep on getting warnings in tests to do this, it's the updated version of the code below
+    # class Config:           Old code that won't work in Pydantic v3 apparently
+    #     env_file = ".env"
+    # Please Ensure to test This in the PR, need to make sure it works
 settings = Settings()
