7b06a1f0afd0e3f92b97087c2540437063cee998
Mihaly Toth-Tarsoly <me@misi.me>
2026-01-02 10:51:13 +0000

    Update sidebar

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,8 +1,8 @@
 "use server"
-import { Suspense } from "react";
 import {
     Sidebar,
-    SidebarContent, SidebarGroup,
+    SidebarContent,
+    SidebarGroup,
     SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
@@ -17,10 +17,9 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import {BookCheck, BookText, ChartLine, LayoutDashboard, Settings, SwatchBook, User} from "lucide-react";
+import {ArrowUpDown, BookCheck, BookText, ChartLine, LayoutDashboard, Settings, SwatchBook, User} from "lucide-react";
 import LogoutButton from "@/components/logout-button";
-import {auth} from "@/lib/auth";
-import {headers} from "next/headers";
+import {requireSession} from "@/lib/auth-utils";
 
 const adminItems = [
     {
@@ -83,130 +82,130 @@ const studentItems = [
 ];
 
 
-
 export default async function AppSidebar() {
-    const s = await auth.api.getSession(
-        {headers: await headers()}
-    );
-    if (!s) {
-        return <></>
+    const s = await requireSession();
+    let type = s.user.role;
+    if (!type) {
+        type = "user";
     }
-    const type = s.user.role;
     const items = type === "lecturer" ? adminItems : studentItems;
 
     return (
-    <Suspense>
-            <Sidebar variant="floating">
-                <SidebarHeader>
-                    <SidebarMenu>
-                        <SidebarMenuItem className="py-4 md:py-0">
-                            <SidebarMenuButton
-                                className="size-20 w-full justify-center md:justify-center md:aspect-square"
-                                asChild
-                            >
-                                <div>
-                                    <Image
-                                        src={`/images/avon-white-optimized.svg`}
-                                        alt="logo"
-                                        width={200}
-                                        height={200}
-                                        loading={"eager"}
-                                        className="dark:block hidden p-4 md:p-0"
-                                    />
-                                    <Image
-                                        src={`/images/avon-black-optimized.svg`}
-                                        alt="logo"
-                                        width={200}
-                                        height={200}
-                                        className="dark:hidden p-4 md:p-0"
-                                    />
-                                </div>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    </SidebarMenu>
-                </SidebarHeader>
-                <SidebarContent>
-                    <SidebarGroup className="h-full">
-                        <SidebarMenu className="flex flex-col justify-center md:justify-between h-full">
-                            <div className="flex flex-row flex-wrap justify-center items-center md:flex-col ">
-                                {items
-                                    .filter((item) => !item.bottom)
-                                    .map((item) => (
-                                        <SidebarMenuItem
-                                            key={item.title}
-                                            className="aspect-square md:flex-1 md:!h-full md:!w-full"
-                                        >
-                                            <SidebarMenuButton className="h-full" asChild>
-                                                <Link
-                                                    href={item.url}
-                                                    className="flex flex-col justify-center items-center !h-32 !w-32 md:!h-full md:!w-full aspect-square"
-                                                >
-                                                    <item.icon strokeWidth={1} className="!size-8" />
-                                                    <span className="text-accent-foreground">
-                          {item.title}
-                        </span>
-                                                </Link>
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    ))}
+        <Sidebar variant="floating">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem className="md:py-0">
+                        <SidebarMenuButton
+                            className="size-25 w-full justify-center md:justify-center"
+                            asChild
+                        >
+                            <div>
+                                <Image
+                                    src={`/images/avon-white-optimized.svg`}
+                                    alt="logo"
+                                    width={100}
+                                    height={100}
+                                    loading={"eager"}
+                                    className="dark:block hidden p-4 md:p-0"
+                                />
+                                <Image
+                                    src={`/images/avon-black-optimized.svg`}
+                                    alt="logo"
+                                    width={100}
+                                    height={100}
+                                    className="dark:hidden p-4 md:p-0"
+                                />
                             </div>
-
-                            {/* bottom part of sidebar */}
-                            <div className="flex flex-row flex-wrap justify-center items-center md:flex-col">
-                                {items
-                                    .filter((item) => item.bottom)
-                                    .map((item) => (
-                                        <SidebarMenuItem
-                                            key={item.title}
-                                            className="aspect-square md:flex-1 md:!h-full md:!w-full"
-                                        >
-                                            <SidebarMenuButton className="h-full" asChild>
-                                                <Link
-                                                    href={item.url}
-                                                    className="flex flex-col justify-center items-center !h-32 !w-32 md:!h-full md:!w-full aspect-square"
-                                                >
-                                                    <item.icon strokeWidth={1} className="!size-8" />
-                                                    <span className="text-accent-foreground">
-                          {item.title}
-                        </span>
-                                                </Link>
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    ))}
-
-                                <SidebarMenuItem
-                                    key={"Account"}
-                                    className="aspect-square hidden md:flex md:flex-1 md:!h-full md:!w-full"
-                                >
-                                    <SidebarMenuButton
-                                        className="flex aspect-square justify-center align-center !h-full"
-                                        asChild
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup className="h-full">
+                    <SidebarMenu className="flex h-full flex-col md:justify-between">
+                        <div className="flex flex-col gap-4">
+                            {items
+                                .filter((item) => !item.bottom)
+                                .map((item) => (
+                                    <SidebarMenuItem
+                                        key={item.title}
+                                        className="w-full"
                                     >
-                                        <DropdownMenu>
-                                            <DropdownMenuTrigger className="!h-full hover:bg-accent-foreground/10">
-                                                <Link
-                                                    href="#"
-                                                    className="flex flex-col justify-center items-center !h-32 !w-32 md:!h-full md:!w-full aspect-square"
-                                                >
-                                                    <User strokeWidth={1} className="!size-8" />
-                                                    <span className="text-accent-foreground text-sm">
-                          Account
+                                        <SidebarMenuButton asChild>
+                                            <Link
+                                                href={item.url}
+                                                className="flex flex-row"
+                                            >
+                                                <item.icon strokeWidth={1} className="!size-8"/>
+                                                <span className="text-accent-foreground">
+                          {item.title}
                         </span>
-                                                </Link>
-                                            </DropdownMenuTrigger>
-                                            <DropdownMenuContent side="right">
-                                                <DropdownMenuItem>{s.user.name}</DropdownMenuItem>
-                                                <DropdownMenuSeparator />
-                                                <LogoutButton/>
-                                            </DropdownMenuContent>
-                                        </DropdownMenu>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            </div>
-                        </SidebarMenu>
-                    </SidebarGroup>
-                </SidebarContent>
-            </Sidebar>
-    </Suspense>
-  );
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                        </div>
+
+                        {/* bottom part of sidebar */}
+                        <div className="flex flex-col gap-4 items-start">
+                            {items
+                                .filter((item) => item.bottom)
+                                .map((item) => (
+                                    <SidebarMenuItem
+                                        key={item.title}
+                                        className="w-full"
+                                    >
+                                        <SidebarMenuButton className="h-full" asChild>
+                                            <Link
+                                                href={item.url}
+                                                className="flex flex-row items-center"
+                                            >
+                                                <item.icon strokeWidth={1} className="!size-8"/>
+                                                <span className="text-accent-foreground">
+                          {item.title}
+                        </span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            <SidebarMenuItem
+                                key={"Account"}
+                                className="flex flex-col gap-4 items-start w-full"
+                            >
+                                <SidebarMenuButton
+                                    className="h-full w-full"
+                                    asChild
+                                >
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger className="w-full hover:bg-accent-foreground/10">
+                                            <Link
+                                                href="#"
+                                                className="flex flex-row w-full items-start h-full"
+                                            >
+                                                <User strokeWidth={1} className="!size-10"/>
+                                                <div className={"flex flex-col w-full items-start"}>
+                                                    <span className="text-accent-foreground text-sm">
+                                                  {s.user.name}
+                                                </span>
+                                                    <span className="text-muted-foreground text-sm">
+                                                  {type.charAt(0).toUpperCase() + type.slice(1)}
+                                                </span>
+                                                </div>
+                                                <ArrowUpDown strokeWidth={2} className={"self-center"}/>
+                                            </Link>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent side="right">
+                                            <DropdownMenuItem>{s.user.name}</DropdownMenuItem>
+                                            <DropdownMenuSeparator/>
+                                            <LogoutButton/>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </div>
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    );
 }
