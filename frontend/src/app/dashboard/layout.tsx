8196e9e8d567c79c2abae1771aaa1fbb852df38b
Josh Jenkins <josshuaj7@gmail.com>
2026-01-13 11:41:02 +0000

    Moving buttons on top bar and resizing correctly

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -14,11 +14,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4 bg-sidebar md:bg-transparent border-b md:border-0">
           <div className="flex flex-row gap-2 items-center">
-            <ModeToggle />
             <SidebarTrigger className="-ml-1" />
-            <p className="font-normal text-xl">Lecturer Dashboard</p>
+            <p className="font-normal text-xl">Courseworks</p>
           </div>
-          <NotificationBar></NotificationBar>
+          <div className="flex flex-row gap-2 items-center">
+            <ModeToggle />
+            <NotificationBar></NotificationBar>
+          </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 px-4">{children}</div>
       </SidebarInset>
--- frontend/src/app/units/layout.tsx
+++ frontend/src/app/units/layout.tsx
@@ -18,11 +18,13 @@ export default async function Layout({
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4 bg-sidebar md:bg-transparent border-b md:border-0">
           <div className="flex flex-row gap-2 items-center">
-            <ModeToggle />
             <SidebarTrigger className="-ml-1" />
-            <p className="font-normal text-xl">Units</p>
+            <p className="font-normal text-xl">Courseworks</p>
           </div>
-          <NotificationBar></NotificationBar>
+          <div className="flex flex-row gap-2 items-center">
+            <ModeToggle />
+            <NotificationBar></NotificationBar>
+          </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 px-4 mt-4">{children}</div>
       </SidebarInset>
