b9fcacc0315184eac1fbc3c5b07e730859175bef
Jack Dempsey <jack.william.dempsey@gmail.com>
2026-01-18 20:43:51 +0000

    delete, update and types tests

--- backend/tests/model/test_programme_model.py
+++ backend/tests/model/test_programme_model.py
@@ -51,3 +51,57 @@ def test_programme_query_by_programme_id(session):
 
     assert result is not None
     assert result.id == programme_id
+
+# Testing we can update the model correctly
+def test_programme_update(session):
+    start = date.today()
+    end = start + timedelta(days=365)
+    programme_id = uuid4()
+
+    programme = Programme(id=programme_id, name="Year 2026/2027", start_date=start, end_date=end)
+
+    session.add(programme)
+    session.commit()
+    session.refresh(programme)
+    programme.name = "Year 2027/2028"
+    session.add(programme)
+    session.commit()
+    session.refresh(programme)
+
+    assert programme.name == "Year 2027/2028"
+
+
+# Testing we can successfully delete a programme
+def test_programme_delete(session):
+    start = date.today()
+    end = start + timedelta(days=365)
+    programme_id = uuid4()
+
+    programme = Programme(id=programme_id, name="Year 2026/2027", start_date=start, end_date=end)
+
+    session.add(programme)
+    session.commit()
+    session.refresh(programme)
+
+    session.delete(programme)
+    session.commit()
+
+    assert session.get(Programme, programme.id) is None
+
+
+# Make sure field types are correct
+def test_programme_field_types(session):
+    start = date.today()
+    end = start + timedelta(days=365)
+    programme_id = uuid4()
+
+    programme = Programme(id=programme_id, name="Year 2026/2027", start_date=start, end_date=end)
+
+    session.add(programme)
+    session.commit()
+    session.refresh(programme)
+
+    assert isinstance(programme.id, UUID)
+    assert isinstance(programme.name, str)
+    assert isinstance(programme.start_date, date)
+    assert isinstance(programme.end_date, date)
