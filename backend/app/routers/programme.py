e2a2c8a2e6d0df6aec5929cd757ccc00cbe3aded
Jack Dempsey <jack.william.dempsey@gmail.com>
2026-01-21 22:25:30 +0000

    get

---
+++ backend/app/routers/programme.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, status
+from app.db.session import get_session
+from sqlmodel import Session
+
+from app.models.programme import Programme
+from app.schemas.programme import ProgrammeCreate, ProgrammeRead
+
+router = APIRouter(prefix="/programmes", tags=["programmes"])
+session_dependency = Annotated[Session, Depends(get_session)]
+
+
+@router.post('/create', response_model = ProgrammeRead, status_code=status.HTTP_201_CREATED)
+async def create_programme(programme: ProgrammeCreate, session: session_dependency):
+    db_programme = Programme(
+        name=programme.name,
+        start_date=programme.start_date,
+        end_date=programme.end_date,
+    )
+
+    session.add(db_programme)
+    session.commit()
+    session.refresh(db_programme)
+
+    return db_programme
---
+++ backend/app/schemas/programme.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+from uuid import UUID
+from datetime import date
+
+from pydantic import BaseModel, AfterValidator
+
+def is_valid_name(name: str) -> str:
+    name = name.strip()
+    if 1 <= len(name) <= 100:
+        return name
+    raise ValueError("Name must be between 1 and 100 characters")
+
+
+def is_valid_date(value: date) -> date:
+    today = date.today()
+
+    if value <= today:
+        raise ValueError("Start date must be in the future")
+
+    return value
+
+# Can't test for end_date > start_date here as we don't have access to both fields in teh validator, so check in route handler (and create form on frontend inforces it anyway)
+
+Name = Annotated[str, AfterValidator(is_valid_name)]
+StartDate = Annotated[date, AfterValidator(is_valid_date)]
+EndDate = Annotated[date, AfterValidator(is_valid_date)]
+
+class ProgrammeCreate(BaseModel):
+    name: Name
+    start_date: StartDate
+    end_date: EndDate
+
+
+class ProgrammeRead(BaseModel):
+    id: UUID
+    name: Name
+    start_date: StartDate
+    end_date: EndDate
+    units: list
