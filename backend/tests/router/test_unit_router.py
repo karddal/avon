eee06c5bf8297c1fca8c7acdc1727a386e68aed3
Hrushikesh Emkay <emkayhrushikesh@gmail.com>
2026-01-22 12:33:55 +0000

    Wrote tests to assert that you can get the unit details when provided valid details and you dont get them if you provide invalid details

--- backend/tests/router/test_unit_router.py
+++ backend/tests/router/test_unit_router.py
@@ -18,8 +18,14 @@ def create_programme(session) -> UUID:
     # unit = Unit(id=unit_id, name="Test Unit", description="Test description", unit_code="COMS20017", colour="abcdef", programme_id=programme.id,)
     # session.add(unit)
     # session.commit()
-
     return programme.id
+
+def create_unit(session, programme_id) -> UUID:
+    unit_id = uuid4()
+    unit = Unit(id=unit_id, name="Test Unit", description="Test description", unit_code="COMS20017", colour="abcdef", programme_id=programme_id,)
+    session.add(unit)
+    session.commit()
+    return unit_id
 
 def valid_unit_payload(programme_id):
     return {
@@ -67,7 +73,7 @@ def test_create_valid_unit(client, session):
     units = session.exec(statement).all()
 
     assert len(units) == 1
-        
+    assert units[0].id is not None    
     assert str(units[0].programme_id) == data["programme_id"]
 
 # Invalid test
@@ -97,8 +103,29 @@ def test_invalid_programme_id(client, session):
 
     assert response.status_code == 400
 
-# Tests to get unit details
+## Tests to get unit details
+# Tests to get unit with valid details
+def test_get_valid_unit_details(client, session):
+    programme_id = create_programme(session)
+    unit_id = create_unit(session, programme_id)
+    response = client.get("/units/" + str(unit_id))
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["name"] == "Test Unit"
+    assert data["unit_code"] == "COMS20017"
+    assert data["programme_id"] == str(programme_id)
+
+# Test to get unit with invalid details
+def test_get_invalid_unit_details(client, session):
+    invalid_unit_id = "bec07dbc-08aa-4b26-b1c7-aed9e13496cb"
+    response = client.get("/units/"+invalid_unit_id)
+
+    assert response.status_code == 404
+
 # Tests to get units with dates
+
+
 # Tests to get the lecturers of the units
 # Tests to update units
 # Tests to delete units
