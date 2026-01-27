ede45529b40358b442c0daa2f994f1ba069e7d70
polo233473 <37165099+polo233473@users.noreply.github.com>
2026-01-27 18:44:48 +0000

    Create calendar-dashboard.tsx

---
+++ frontend/src/components/calendar/calendar-dashboard.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import {useCallback, useState} from "react";
+import {CalendarNavigationCard} from "@/components/calendar/calendar-navigation-card";
+import {Tabs, TabsContent} from "@/components/ui/tabs";
+import {WeeklyTimeTableCard} from "@/components/calendar/week-timetable-card";
+
+export default function CalendarDashboard() {
+    const [weekStartDate, setWeekStartDate] = useState<Date>(new Date())
+    const [unitIds, setUnitIds] = useState<string[]>([])
+
+    const onUnitIdsChange = useCallback((ids: string[]) =>{
+        setUnitIds(ids)
+    }, [])
+
+    return (
+        <Tabs defaultValue="timetable">
+            <CalendarNavigationCard
+                weekStartDate={weekStartDate}
+                onWeekStartDateChange={setWeekStartDate}
+                onUnitIdsChange={onUnitIdsChange}
+            />
+            
+            <TabsContent value="timetable">
+                <WeeklyTimeTableCard
+                    weekStartDate={weekStartDate}
+                    unitIds={unitIds}
+                />
+            </TabsContent>
+
+            <TabsContent value="events">
+
+            </TabsContent>
+        </Tabs>
+
+    )
+}
