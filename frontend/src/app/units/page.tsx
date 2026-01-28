f3e08f884149440979a45d27bfb01538c42eb0b6
Hrushikesh Emkay <emkayhrushikesh@gmail.com>
2026-01-28 17:17:33 +0000

    Made the button nicer and aligned it

--- frontend/src/app/units/page.tsx
+++ frontend/src/app/units/page.tsx
@@ -5,34 +5,62 @@ import Loading from "@/app/coursework/loading";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import UnitList from "@/components/units/unit-list";
 import { requireSession } from "@/lib/auth-utils";
+import { Button } from "@/components/ui/button";
+import { ClipboardPlus } from "lucide-react";
 
-function PageContent() {
+type UserRole = {
+  userRole: string
+}
+
+function CreateUnit({ userRole }: UserRole) {
   return (
-    <div className="space-y-6">
-      <Tabs defaultValue="ongoing">
-        <TabsList className="flex flex-row gap-4 bg-background my-4">
-          <div className="bg-accent p-1">
-            <TabsTrigger value="ongoing" className="bg-accent px-4 py-2">
-              Ongoing
-            </TabsTrigger>
-            <TabsTrigger value="finished" className="bg-accent px-4 py-2">
-              Finished
-            </TabsTrigger>
+    <div>
+      {
+        userRole === "admin" && (
+          <Button asChild variant="outline" size="sm" className="mt-2">
+            <Link href={"/units/create-unit/"}>
+              <ClipboardPlus />
+              Add Unit
+            </Link>
+          </Button>
+        )
+      }
+    </div>
+  )
+}
+
+function PageContent({ userRole }: UserRole) {
+  return (
+    <div className="">
+      <div className="space-y-6">
+        <Tabs defaultValue="ongoing">
+          <div className="flex flex-row align-middle items-center justify-between">
+            <TabsList className="flex flex-row gap-4 bg-background my-4">
+              <div className="bg-accent p-1">
+                <TabsTrigger value="ongoing" className="bg-accent px-4 py-2">
+                  Ongoing
+                </TabsTrigger>
+                <TabsTrigger value="finished" className="bg-accent px-4 py-2">
+                  Finished
+                </TabsTrigger>
+              </div>
+            </TabsList>
+            <CreateUnit userRole={userRole} />
           </div>
-        </TabsList>
 
-        <TabsContent value="ongoing">
-          <Suspense fallback={<Loading />}>
-            <UnitList finished={false} />
-          </Suspense>
-        </TabsContent>
+          <TabsContent value="ongoing">
+            <Suspense fallback={<Loading />}>
+              <UnitList finished={false} />
+            </Suspense>
+          </TabsContent>
 
-        <TabsContent value="finished">
-          <Suspense fallback={<Loading />}>
-            <UnitList finished={true} />
-          </Suspense>
-        </TabsContent>
-      </Tabs>
+          <TabsContent value="finished">
+            <Suspense fallback={<Loading />}>
+              <UnitList finished={true} />
+            </Suspense>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
@@ -45,14 +73,15 @@ async function AdminPage() {
   }
   return (
     <div>
-      {userRole === "admin" && (
-        <Link href={"/units/create-unit"}>
-          <div className="w-32 py-4 border-2 hover:cursor-pointer">
-            Create unit
-          </div>
-        </Link>
-      )}
-      <PageContent />
+      {/* {userRole === "admin" && (
+        <Button asChild variant="outline" size="sm" className="mt-2">
+          <Link href={{ pathname: "/programmes/create_programme" }}>
+            <ClipboardPlus />
+            Add Unit
+          </Link>
+        </Button>
+      )} */}
+      <PageContent userRole={userRole} />
     </div>
   )
 }
