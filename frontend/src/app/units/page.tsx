d4a874dd795ab24a8eeb4085bcbf54f9aaf149a4
Hrushikesh Emkay <emkayhrushikesh@gmail.com>
2026-01-28 14:03:41 +0000

    linter changes and fixed merge conflicts

--- frontend/src/app/units/page.tsx
+++ frontend/src/app/units/page.tsx
@@ -3,13 +3,44 @@
 import Link from "next/link";
 import { Suspense } from "react";
 import Loading from "@/app/coursework/loading";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import UnitList from "@/components/units/unit-list";
 import { requireSession } from "@/lib/auth-utils";
 
 async function PageContent() {
-  await requireSession(); // make sure logged in
+  const s = await requireSession(); // make sure logged in
+  let userRole = s.user.role;
+  if (!userRole) {
+    userRole = "user";
+  }
+  return (
+    <div className="space-y-6">
+      <Tabs defaultValue="ongoing">
+        <TabsList className="flex flex-row gap-4 bg-background my-4">
+          <div className="bg-accent p-1">
+            <TabsTrigger value="ongoing" className="bg-accent px-4 py-2">
+              Ongoing
+            </TabsTrigger>
+            <TabsTrigger value="finished" className="bg-accent px-4 py-2">
+              Finished
+            </TabsTrigger>
+          </div>
+        </TabsList>
 
-  return <UnitList></UnitList>;
+        <TabsContent value="ongoing">
+          <Suspense fallback={<Loading />}>
+            <UnitList finished={false} />
+          </Suspense>
+        </TabsContent>
+
+        <TabsContent value="finished">
+          <Suspense fallback={<Loading />}>
+            <UnitList finished={true} />
+          </Suspense>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default async function UnitPage() {
