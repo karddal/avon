b6f66f5e2674458917cfa4fc770cdaf48686ebd6
Josh Jenkins <josshuaj7@gmail.com>
2026-02-02 18:27:04 +0000

    Fixing Sidebar from not reloading by caching requireSession

--- frontend/src/components/sidebar/app-sidebar-wrapper.tsx
+++ frontend/src/components/sidebar/app-sidebar-wrapper.tsx
@@ -1,20 +1,12 @@
-"use server";
-import { Suspense } from "react";
 import Jump from "@/components/navi/jump-wrapper";
+import AppSidebarContent from "@/components/sidebar/app-sidebar-content";
 import { Sidebar } from "@/components/ui/sidebar";
-import AppSideBarContent from "./app-sidebar-content";
-import SidebarLoading from "./sidebar-loading";
 
 export default async function AppSidebar() {
   return (
     <Sidebar variant="floating">
-      <Suspense fallback={<SidebarLoading />}>
-        <AppSideBarContent></AppSideBarContent>
-      </Suspense>
+      <AppSidebarContent></AppSidebarContent>
       <Jump />
     </Sidebar>
   );
-}
-function _requireSession() {
-  throw new Error("Function not implemented.");
 }
