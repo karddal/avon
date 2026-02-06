5a04bd038ba4d4b5a60a9f1a98c4e5cf499b8a8b
Josh Jenkins <josshuaj7@gmail.com>
2026-02-06 21:40:50 +0000

    changing timer on information.tsx

--- frontend/src/app/coursework/[slug]/page.tsx
+++ frontend/src/app/coursework/[slug]/page.tsx
@@ -3,7 +3,6 @@ import CourseworkLectDropdown from "@/components/coursework/coursework-lect-drop
 import Repository from "@/components/coursework/repository";
 import { DropdownCard } from "@/components/dropdown-card";
 import RunTestsItem from "@/components/run-tests-item";
-import TestPassedProgressBar from "@/components/tests-passed-progress";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getRequestJWT, requireSession } from "@/lib/auth-utils";
@@ -130,58 +129,13 @@ async function CourseworkPageContent({
             <CourseworkInformation slug={slug} token={token} />
           </Suspense>
           <DropdownCard
-            openByDefault={false}
+            openByDefault={true}
             title="Tools"
             desc="Tools you can use for this coursework appear here."
           >
             {" "}
             <RunTestsItem />
           </DropdownCard>
-          <Card className="flex flex-col gap-4 h-96 md:h-128 lg:min-h-0 xl:h-auto xl:min-h-0">
-            <CardHeader>
-              <CardTitle>
-                <div className="text-2xl">Activity</div>
-                <div className="font-light">
-                  See a feed of events related to your coursework here.
-                </div>
-              </CardTitle>
-              {/*<Tabs defaultValue="account">*/}
-              {/*  <TabsList>*/}
-              {/*    <TabsTrigger value="account">Ongoing</TabsTrigger>*/}
-              {/*    <TabsTrigger value="password">Finished</TabsTrigger>*/}
-              {/*  </TabsList>*/}
-              {/*</Tabs>*/}
-            </CardHeader>
-
-            <Suspense fallback={<Skeleton />}>
-              <CardContent className="overflow-y-scroll">
-                <div className="flex flex-col gap-2">
-                  <Card className="gap-2 p-4 bg-accent w-full text-2xl font-normal">
-                    <p>Tests finished</p>
-                    <TestPassedProgressBar
-                      className=""
-                      value={100}
-                      colour="#59AC77"
-                    />
-                    <p className="text-sm">100/100 tests passed.</p>
-                    <p className="text-sm font-light">20 October 2025 15:00</p>
-                    {/*<div className="w-full h-1 bg-green-600"></div>*/}
-                  </Card>
-                  <Card className="gap-2 p-4 bg-accent w-full text-2xl font-normal">
-                    <p>Tests finished</p>
-                    <TestPassedProgressBar
-                      className=""
-                      value={35}
-                      colour="#F4991A"
-                    />
-                    <p className="text-sm">35/100 tests passed.</p>
-                    <p className="text-sm font-light">20 October 2025 15:00</p>
-                    {/*<div className="w-full h-1 bg-green-600"></div>*/}
-                  </Card>
-                </div>
-              </CardContent>
-            </Suspense>
-          </Card>
         </div>
       </section>
     </>
