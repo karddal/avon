adb05c15ddd7857676cbe85667987b788fdf32f8
Jack Dempsey <jack.william.dempsey@gmail.com>
2026-02-07 00:40:31 +0000

    searching users

---
+++ frontend/src/components/settings/list-users.tsx
@@ -0,0 +1,201 @@
+"use client";
+
+import type { User } from "better-auth";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Menu,
+  Plus,
+  SendHorizonal,
+  TextSearch,
+  UserIcon,
+  X,
+} from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import UserCard from "@/components/user-card";
+import { batch_add_students_to_unit } from "@/lib/actions/batch_add_students_to_unit";
+import { get_lecturers } from "@/lib/actions/get_lecturers";
+import { get_students } from "@/lib/actions/get_students";
+import {
+  type SearchResponse,
+  search_by_name,
+} from "@/lib/actions/search_by_name";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { remove_user_enrollment } from "@/lib/actions/remove_user_enrollment";
+
+function _getInitials(name: string) {
+  if (!name || typeof name !== "string") return "?";
+  const allNames = name.trim().split(" ");
+  if (allNames.length === 0) return "?";
+
+  const first = allNames[0].charAt(0);
+  const last =
+    allNames.length > 1 ? allNames[allNames.length - 1].charAt(0) : "";
+  return (first + last).toUpperCase();
+}
+  
+
+export default function ListMembers() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState<SearchResponse>({
+    users: [],
+    total: 0,
+    limit: 0,
+    offset: 0,
+  });
+  const [offset, setOffset] = useState<number>(0);
+  const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
+  const [length, setLength] = useState<number>(0);
+  const [disabledUsers, setDisabledUsers] = useState<string[]>([]);
+
+  const limit = 5;
+
+  async function showUsers(query: string, offset: number) {
+    setLoading(true);
+    const response: SearchResponse = await search_by_name(
+      query,
+      offset,
+      limit,
+      "user",
+    );
+    setLoading(false);
+    setResponse(response);
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row gap-2">
+        <Input
+          className="w-full"
+          placeholder="Search for someone..."
+          value={searchQuery}
+          onChange={(e) => {
+            setOffset(0);
+            setSearchQuery(e.target.value);
+            showUsers(e.target.value, offset);
+          }}
+        />
+      </div>
+      {loading && searchQuery.length === 0 ? (
+        <div className="flex flex-col items-center gap-4 w-full">
+          <Skeleton className="bg-accent w-full h-12" />
+          <Skeleton className="bg-accent w-full h-12" />
+          <Skeleton className="bg-accent w-full h-12" />
+        </div>
+      ) : (
+        <></>
+      )}
+      {searchQuery.length === 0 ? (
+        <></>
+      ) : (
+        <div className="flex flex-col gap-2 overflow-y-scroll max-h-128 bg-accent p-2">
+          {response.users.length > 0 ? (
+            response.users.map((user: User) => (
+              <div className="group relative w-full" key={user.id}>
+                <UserCard
+                  id={user.id}
+                  name={user.name}
+                  image={user.image}
+                  email={user.email}
+                />
+
+                <div className="absolute top-2 right-2 w-8 h-8">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="bg-card aspect-square! shadow border h-8 w-8 items-center justify-center flex hover:bg-card/50 hover:transition">
+                        <Menu className="p-0"></Menu>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                        <DropdownMenuItem>
+                            <TextSearch></TextSearch>
+                            View Student
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            // onClick={() => handleDelete(user.id)}
+                            className="text-destructive hover:text-destructive"
+                        >
+                            <X className="text-destructive hover:text-destructive"></X>
+                            <p className="text-destructive hover:text-destructive">
+                            Remove Student
+                            </p>
+                        </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+          {!loading && searchQuery.length > 0 && response.users.length === 0 ? (
+            <Empty>
+              <EmptyHeader>
+                <EmptyMedia variant="icon">
+                  <UserIcon />
+                </EmptyMedia>
+                <EmptyTitle>No users found</EmptyTitle>
+                <EmptyDescription className="flex flex-row">
+                  No users for search query "
+                  <div className="truncate max-w-20">{searchQuery}</div>"
+                </EmptyDescription>
+              </EmptyHeader>
+            </Empty>
+          ) : (
+            <></>
+          )}
+          {response.users.length > 0 ? (
+            <div className="flex flex-row items-center text-center my-2">
+              <Button
+                size="icon"
+                variant="outline"
+                disabled={offset / limit + 1 === 1}
+                onClick={() => {
+                  const newOffset = Math.max(0, offset - limit);
+                  setOffset(newOffset);
+                  showUsers(searchQuery, newOffset);
+                }}
+              >
+                <ArrowLeft></ArrowLeft>
+              </Button>
+              <span className="font-light text-center w-full">
+                {offset / limit + 1}/{Math.ceil(response.total / limit)} -
+                Showing {response.users.length} results out of {response.total}
+              </span>
+              <Button
+                size="icon"
+                variant="outline"
+                disabled={
+                  offset / limit + 1 === Math.ceil(response.total / limit)
+                }
+                onClick={() => {
+                  const newOffset = Math.min(
+                    offset + limit,
+                    response.total - 1,
+                  );
+                  setOffset(newOffset);
+                  showUsers(searchQuery, newOffset);
+                }}
+              >
+                <ArrowRight></ArrowRight>
+              </Button>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
--- frontend/src/components/settings/settings-contents.tsx
+++ frontend/src/components/settings/settings-contents.tsx
@@ -1,4 +1,5 @@
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/new_tabs";
+import ListMembers from "@/components/settings/list-users";
 import {KeyRound, Palette, SquareUser} from "lucide-react";
 import AccountSettings from "@/components/settings/account-settings";
 
@@ -14,6 +15,9 @@ export default function SettingsContents() {
                     <Palette />
                     Theme
                 </TabsTrigger>
+                <TabsTrigger value="manage">
+                    User Management
+                </TabsTrigger>
             </TabsList>
             <div className={"no-scrollbar max-h-[50vh] w-full overflow-y-auto p-6"}>
                 <TabsContent value={"account"}>
@@ -21,6 +25,9 @@ export default function SettingsContents() {
                 </TabsContent>
                 <TabsContent value={"notifications"}>
 
+                </TabsContent>
+                <TabsContent value={"manage"}>
+                    <ListMembers/>
                 </TabsContent>
             </div>
         </Tabs>
