13114de1151b5ff68adedd04250c748db8af7974
polo233473 <37165099+polo233473@users.noreply.github.com>
2026-02-09 13:58:42 +0000

    more ws forgot to delete

--- backend/app/routers/unit.py
+++ backend/app/routers/unit.py
@@ -7,12 +7,11 @@ from sqlmodel import Session, select
 from sqlalchemy.orm.strategy_options import selectinload
 from sqlalchemy import or_
 
-from app.core.security import get_current_user, get_current_user_with_role
+from app.core.security import get_current_user_with_role
 from app.db.session import get_session
 from app.models.programme import Programme
 from app.models.unit import Unit
 from app.models.unit_enrollment import UnitEnrollment
-from app.routers.websocket import manager
 from app.schemas.security import CurrentUser
 from app.schemas.unit import (
     CourseworkAll,
