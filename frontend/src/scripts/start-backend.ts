a4fa66a11d83cd6c2a98068a46e3e85a80cd08d0
polo233473 <37165099+polo233473@users.noreply.github.com>
2026-02-14 19:33:10 +0000

    put start backend to file

---
+++ frontend/src/scripts/start-backend.ts
@@ -0,0 +1,21 @@
+import { spawn } from "node:child_process";
+import path from "node:path";
+import dotenv from "dotenv"
+
+const backendDir = path.resolve(process.cwd(), "../backend");
+
+dotenv.config({
+    path: path.join(backendDir, ".env.dev"),
+})
+
+const backend = spawn("uv", ["run", "fastapi", "dev"], {
+  cwd: backendDir,
+  stdio: "inherit",
+  env: {
+    ...process.env,
+    IGNORE_AUTH: "True",
+    TESTING_MODE: "True",
+  },
+});
+
+backend.on("exit", (code) => process.exit(code ?? 1));
