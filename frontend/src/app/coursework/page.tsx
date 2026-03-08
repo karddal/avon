8ca5e4e48c140604cef79a7c8ab6828ea4255f5c
polo233473 <37165099+polo233473@users.noreply.github.com>
2026-03-08 16:25:14 +0000

    improve ui

--- frontend/src/app/coursework/page.tsx
+++ frontend/src/app/coursework/page.tsx
@@ -4,27 +4,40 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Loading from "./loading";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
+import {ClipboardPlus} from "lucide-react";
+
+function CreateCoursework() {
+    return (
+        <div>
+            <Button asChild variant="outline" size="sm" className="mt-2">
+                <Link href={"/coursework/create-coursework"}>
+                    <ClipboardPlus />
+                    Create coursework
+                </Link>
+            </Button>
+        </div>
+    )
+}
 
 async function PageContent() {
   return (
     <div className="space-y-6">
-        <div className="flex items-center justify-end pt-4">
-            <Link href="/coursework/create-coursework">
-                <Button>Create coursework</Button>
-            </Link>
-        </div>
 
         <Tabs defaultValue="ongoing" className={""}>
-        <TabsList className="flex flex-row gap-4 bg-background my-4">
-          <div className="bg-accent p-1">
-            <TabsTrigger value="ongoing" className="bg-accent px-4 py-2">
-              Ongoing
-            </TabsTrigger>
-            <TabsTrigger value="finished" className="bg-accent px-4 py-2">
-              Finished
-            </TabsTrigger>
-          </div>
-        </TabsList>
+            <div className="flex flex-row align-middle items-center justify-between">
+                <TabsList className="flex flex-row gap-4 bg-background my-4">
+                    <div className="bg-accent p-1">
+                        <TabsTrigger value="ongoing" className="bg-accent px-4 py-2">
+                            Ongoing
+                        </TabsTrigger>
+                        <TabsTrigger value="finished" className="bg-accent px-4 py-2">
+                            Finished
+                        </TabsTrigger>
+                    </div>
+                </TabsList>
+                <CreateCoursework />
+            </div>
+
         <TabsContent value="ongoing" className="">
           <Suspense fallback={<Loading />}>
             <CourseworkList finished={false} />
