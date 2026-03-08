2c8b4b825bba8eab609f6e2354cc08521bf1e477
polo233473 <37165099+polo233473@users.noreply.github.com>
2026-03-08 15:44:53 +0000

    new create coursework

---
+++ frontend/src/app/coursework/create-coursework/page.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import {IntForm, UnitOption} from "./form";
+import { getRequestJWT } from "@/lib/auth-utils";
+
+async function Actual() {
+  const token = await getRequestJWT();
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/units/units`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      cache: "no-cache",
+    },
+  );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch units.");
+    }
+
+    const units: UnitOption[] = await response.json();
+  return (
+      <IntForm units={units} />
+  );
+}
+
+export default async function CreateCourseworkFlow() {
+  return (
+    <Suspense>
+      <Actual />
+    </Suspense>
+  );
+}
