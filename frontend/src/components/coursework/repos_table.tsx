de1ddda4af9c511751656e66eea7f4917cef35b5
Mihály Tóth-Tarsoly <me@misi.me>
2026-03-18 21:22:38 +0000

    Add loading state

--- frontend/src/components/coursework/repos_table.tsx
+++ frontend/src/components/coursework/repos_table.tsx
@@ -20,18 +20,18 @@ import { Input } from "../ui/input";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import {columns} from "@/components/coursework/columns";
 import {get_student_repos, StudentNameAndRepo, StudentsRepositoriesResp} from "@/lib/actions/get_student_repos";
-
-const data: StudentNameAndRepo[] = [];
+import {Spinner} from "@/components/ui/spinner";
 
 export function StudentReposTable({coursework_id}: {coursework_id: string}) {
   const [data, setData] = useState<StudentNameAndRepo[]>([]);
-
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect((() => {
+    setLoading(true)
     const updateData = async () => {
       const updatedData = await get_student_repos({coursework_id: coursework_id});
       setData(updatedData.repos)
     }
-    updateData();
+    updateData().then(() => setLoading(false))
   }), [coursework_id])
   const [sorting, setSorting] = React.useState<SortingState>([])
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
@@ -60,92 +60,100 @@ export function StudentReposTable({coursework_id}: {coursework_id: string}) {
     },
   })
 
+  if (loading) {
+    return (
+        <div className={"w-full flex items-center justify-center"}>
+          <Spinner className={"w-10 h-10"}/>
+        </div>
+    )
+  }
   return (
-      <div className="w-full">
-        <div className="flex items-center py-4">
-          <Input
-              placeholder="Filter students..."
-              value={(table.getColumn("student")?.getFilterValue() as string) ?? ""}
-              onChange={(event) =>
-                  table.getColumn("student")?.setFilterValue(event.target.value)
-              }
-              className=""
-          />
-        </div>
-        <div className="overflow-hidden rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                          <TableHead key={header.id}>
-                            {header.isPlaceholder
-                                ? null
-                                : flexRender(
-                                    header.column.columnDef.header,
-                                    header.getContext()
-                                )}
-                          </TableHead>
-                      )
-                    })}
-                  </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                  table.getRowModel().rows.map((row) => (
-                      <TableRow
-                          key={row.id}
-                          data-state={row.getIsSelected() && "selected"}
-                      >
-                        {row.getVisibleCells().map((cell) => (
-                            <TableCell key={cell.id}>
-                              {flexRender(
-                                  cell.column.columnDef.cell,
-                                  cell.getContext()
-                              )}
-                            </TableCell>
-                        ))}
-                      </TableRow>
-                  ))
-              ) : (
-                  <TableRow>
-                    <TableCell
-                        colSpan={columns.length}
-                        className="h-24 text-center"
-                    >
-                      No results. Have you provisioned the coursework on GitLab yet?
-                    </TableCell>
-                  </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex-1 text-sm text-muted-foreground">
-            {table.getFilteredSelectedRowModel().rows.length} of{" "}
-            {table.getFilteredRowModel().rows.length} student(s) or group(s) selected.
-          </div>
-          <div className="space-x-2">
-            <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-            >
-              Previous
-            </Button>
-            <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-            >
-              Next
-            </Button>
-          </div>
-        </div>
-      </div>
+            <div className="w-full">
+              <div className="flex items-center py-4">
+                <Input
+                    placeholder="Filter students..."
+                    value={(table.getColumn("student")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("student")?.setFilterValue(event.target.value)
+                    }
+                    className=""
+                />
+              </div>
+              <div className="overflow-y-scroll rounded-md border">
+                <Table>
+                  <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                          {headerGroup.headers.map((header) => {
+                            return (
+                                <TableHead key={header.id}>
+                                  {header.isPlaceholder
+                                      ? null
+                                      : flexRender(
+                                          header.column.columnDef.header,
+                                          header.getContext()
+                                      )}
+                                </TableHead>
+                            )
+                          })}
+                        </TableRow>
+                    ))}
+                  </TableHeader>
+
+                  <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                            >
+                              {row.getVisibleCells().map((cell) => (
+                                  <TableCell key={cell.id}>
+                                    {flexRender(
+                                        cell.column.columnDef.cell,
+                                        cell.getContext()
+                                    )}
+                                  </TableCell>
+                              ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                          <TableCell
+                              colSpan={columns.length}
+                              className="h-24 text-center"
+                          >
+                            No results. Have you provisioned the coursework on GitLab yet?
+                          </TableCell>
+                        </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </div>
+              <div className="flex items-center justify-end space-x-2 py-4">
+                <div className="flex-1 text-sm text-muted-foreground">
+                  {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                  {table.getFilteredRowModel().rows.length} student(s) or group(s) selected.
+                </div>
+                <div className="space-x-2">
+                  <Button
+                      variant="outline"
+                      size="sm"
+                      onClick={() => table.previousPage()}
+                      disabled={!table.getCanPreviousPage()}
+                  >
+                    Previous
+                  </Button>
+                  <Button
+                      variant="outline"
+                      size="sm"
+                      onClick={() => table.nextPage()}
+                      disabled={!table.getCanNextPage()}
+                  >
+                    Next
+                  </Button>
+                </div>
+              </div>
+            </div>
   )
 }
