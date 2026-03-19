1dd38448d9ba8f28492e486ccaf8deb78e396f56
polo233473 <37165099+polo233473@users.noreply.github.com>
2026-03-19 14:11:42 +0000

    add a test for the create coursework function

--- frontend/cypress/e2e/courseworks_page_admin.cy.ts
+++ frontend/cypress/e2e/courseworks_page_admin.cy.ts
@@ -57,6 +57,40 @@ describe("Coursework listing page - admin tests", () => {
       "Imperative and Functional Programming 2024-2025",
     ).click();
     cy.get("p").should("contain", "Power to the People in 2024");
+  it("Admin can create coursework", () => {
+    const courseworkName = `Cypress Created Coursework ${Date.now()}`;
+    const courseworkSelectorName = courseworkName
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+
+    cy.visit("/coursework");
+    cy.getByCy("create-coursework-link").click();
+    cy.getByCy("create-coursework-unit-option-coms20006-software-engineering-project").then(
+      ($option) => {
+        const unitValue = $option.val() as string | undefined;
+        expect(unitValue).to.be.a("string");
+        expect(unitValue).to.not.equal("");
+        cy.getByCy("create-coursework-unit").select(unitValue!);
+      },
+    );
+    cy.getByCy("create-coursework-selected-unit").should("be.visible");
+    cy.getByCy("create-coursework-name").type(courseworkName);
+    cy.getByCy("create-coursework-description").type(
+      "Coursework created by the admin Cypress flow.",
+    );
+    cy.getByCy("create-coursework-next-step-1").click();
+    cy.getByCy("create-coursework-next-step-2").click();
+    cy.getByCy("create-coursework-submit").click();
+
+    cy.url().should("include", "/coursework");
+    cy.url().should("not.include", "/create-coursework");
+    cy.getByCy("coursework-unit-tab-software-engineering-project-2025-2026").click();
+    cy.getByCy(
+      `coursework-link-software-engineering-project-2025-2026-${courseworkSelectorName}`,
+    ).should(
+      "be.visible",
+    );
   });
 
   // Deletion
