5852793d1f5a2e038ad805a32b9ea48f8fb58de4
Mihály Tóth-Tarsoly <me@misi.me>
2026-04-15 11:31:48 +0100

    lints

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
-from app.models.student_repo import StudentRepo
-from app.models.coursework import Coursework
-from app.models.base_image import BaseImage
+from app.models.student_repo import StudentRepo  # noqa: F401
+from app.models.coursework import Coursework  # noqa: F401
+from app.models.base_image import BaseImage  # noqa: F401
+
