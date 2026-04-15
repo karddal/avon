5e81922574930beab54ecda2ac39a898712e0f2b
Mihály Tóth-Tarsoly <me@misi.me>
2026-04-15 18:12:36 +0100

    respond to pr comments

--- backend/app/sqs_worker.py
+++ backend/app/sqs_worker.py
@@ -12,6 +12,7 @@ from app.notifications.notification import write_notification
 
 logger = logging.getLogger(name="SQS Worker")
 
+
 async def process_message(msg, db: Session):
     body = json.loads(msg["Body"])
     build_id = body["build_id"]
@@ -23,7 +24,7 @@ async def process_message(msg, db: Session):
         logger.error(f"No such run: {build_id}")
         return
 
-    if (exit_code != 0):
+    if exit_code != 0:
         run.status = "failed"
     else:
         run.status = "succeeded"
@@ -37,7 +38,7 @@ async def process_message(msg, db: Session):
             test_run_id=UUID(build_id),
             exit_code=int(exit_code),
             log_s3_uri=s3_key,
-            received_at=datetime.now(timezone.utc)
+            received_at=datetime.now(timezone.utc),
         )
         db.add(db_result)
 
@@ -50,30 +51,37 @@ async def process_message(msg, db: Session):
     # Check whether there are any other running items in the batch
     # If so, send a notification if notifications are enabled.
 
-    if len(
+    if (
+        len(
             db.exec(
-                select(TestRun)
-                        .where(
-                    (TestRun.batch_id == run.batch_id) &
-                    (TestRun.status == "running" or TestRun.status == "pending"))
-            ).all()) == 0:
-                # There are no pending or running, so batch is complete.
+                select(TestRun).where(
+                    (TestRun.batch_id == run.batch_id)
+                    & (TestRun.status == "running" or TestRun.status == "pending")
+                )
+            ).all()
+        )
+        == 0
+    ):
+        # There are no pending or running, so batch is complete.
 
-                succeeded = len(db.exec(
-                select(TestRun)
-                        .where(
-                    (TestRun.batch_id == run.batch_id) &
-                    (TestRun.status == "succeeded"))
-            ).all())
+        succeeded = len(
+            db.exec(
+                select(TestRun).where(
+                    (TestRun.batch_id == run.batch_id) & (TestRun.status == "succeeded")
+                )
+            ).all()
+        )
 
-                failed = len(db.exec(
-                select(TestRun)
-                        .where(
-                    (TestRun.batch_id == run.batch_id) &
-                    (TestRun.status == "failed" or TestRun.status == "error"))
-            ).all())
+        failed = len(
+            db.exec(
+                select(TestRun).where(
+                    (TestRun.batch_id == run.batch_id)
+                    & ((TestRun.status == "failed") | (TestRun.status == "error"))
+                )
+            ).all()
+        )
 
-                test_run_completed_message = f"""Hey! The test batch you started at {run.created_at.isoformat(timespec='minutes')} for the coursework '{run.coursework.name}' has finished running, and you asked to be notified.
+        test_run_completed_message = f"""Hey! The test batch you started at {run.created_at.isoformat(timespec="minutes")} for the coursework '{run.coursework.name}' has finished running, and you asked to be notified.
 Here's a quick summary:
 - Succeeded runs: {succeeded}
 - Failed runs: {failed}
@@ -82,15 +90,14 @@ Have a great day!
 The Avon Team
 """
 
-                if run.notifications_enabled:
-                    write_notification(
-                        session=db,
-                        recipient=run.started_by,
-                        unit_id=run.coursework.unit_id,
-                        title="Test run completed",
-                        body=test_run_completed_message
-                    )
-
+        if run.notifications_enabled:
+            write_notification(
+                session=db,
+                recipient=run.started_by,
+                unit_id=run.coursework.unit_id,
+                title="Test run completed",
+                body=test_run_completed_message,
+            )
 
 
 async def sqs_worker(s, queue_url: str):
@@ -103,24 +110,21 @@ async def sqs_worker(s, queue_url: str):
         while True:
             try:
                 resp = await sqs.receive_message(
-                    QueueUrl=queue_url,
-                    MaxNumberOfMessages=10,
-                    WaitTimeSeconds=20
+                    QueueUrl=queue_url, MaxNumberOfMessages=10, WaitTimeSeconds=20
                 )
+                messages = resp.get("Messages", [])
+
+                for msg in messages:
+                    try:
+                        await process_message(msg, s)
+
+                        await sqs.delete_message(
+                            QueueUrl=queue_url, ReceiptHandle=msg["ReceiptHandle"]
+                        )
+                    except Exception as e:
+                        print(f"Background worker error: {e}")
+
             except Exception as e:
                 logger.error(f"Could not receive sqs messages: {e}")
-
-            messages = resp.get("Messages", [])
-
-            for msg in messages:
-                try:
-                    await process_message(msg, s)
-
-                    await sqs.delete_message(
-                        QueueUrl=queue_url,
-                        ReceiptHandle=msg["ReceiptHandle"]
-                    )
-                except Exception as e:
-                    print(f"Background worker error: {e}")
 
             await asyncio.sleep(1)
